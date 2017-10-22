362ff4e31a66d62e13acfdd59228d28cb35ab82c
wuming <kai2017@qq.com>
2017-10-22 14:14:11 +0800

    test

--- v2conf.sh
+++ v2conf.sh
@@ -62,59 +62,63 @@ cat > ./v2ray.json<<-EOF
 }
 EOF
 
-echo "请输入端口:"
-read -p "(默认端口: 1110):" vport
+input () {
+  read -p "请输入$2,默认($3):" $1
+}
+
+input vport "端口" 1110
 [ -z "${vport}" ] && vport="1110"
 sed -i "s/10086/${vport}/g" "./v2ray.json"
 
-UUID=$(cat /proc/sys/kernel/random/uuid)
-#UUID=$(cat ./uuid)
+[ -f "/proc/sys/kernel/random/uuid" ] && UUID=$(cat /proc/sys/kernel/random/uuid)
+[ -f "/f/temp/uuid" ] && UUID=$(cat /f/temp/uuid)
 [ -z "${UUID}" ] && UUID="173fa4bb-b916-4943-b8d4-e8e37529a9f6"
-echo "请输入UUID:"
-read -p "(默认UUID: ${UUID}):" vuuid
+input vuuid "UUID" ${UUID}
 [ -z "${vuuid}" ] && vuuid=${UUID}
 sed -i "s/23ad6b10-8d1a-40f7-8ad0-e3e35cd38297/${UUID}/g" "./v2ray.json"
 
-echo "请输入websocket路径(建议直接回车用默认:ws)"
-read -p "(默认ws):" v_ws
-[ -z "${v_ws}"] && v_ws="ws"
+input v_ws "websocket路径" ws
+[ -z "${v_ws}" ] && v_ws="ws"
 sed -i "18s/\}/\}\,/g" ./v2ray.json
 sed -i "18a\    \"streamSettings\"\: \{\n      \"network\"\: \"ws\"\,\n      \"wsSettings\"\: \{\n        \"connectionReuse\"\: false\,\n        \"path\"\: \"\/${v_ws}\"\n      \}\n    \}" ./v2ray.json
 
-echo "请输入域名:"
-read -p "示例(abc.com):" vurl
+input vurl "域名,示例(abc.com)" "-" 
 [ -z "${vurl}" ] && echo "错误,没有域名" && exit 1
 
-echo "请输入邮箱:"
-read -p "示例(abc@163.com):" vemail
+input vemail "邮箱,示例(abc@163.com)" "-" 
 [ -z "${vemail}" ] && echo "错误,没有邮箱" && exit 1
 
 cat > ./Caddy.file<<-EOF
 https://${vurl} {
- root /var/www/index
- gzip
- index index.html
- tls ${vemail}
- proxy ${v_ws} localhost:${vport} {
-  websocket
+  root /var/www/index
+  gzip
+  index index.html
+  tls ${vemail}
+  proxy ${v_ws} localhost:${vport} {
+    websocket
   }
 }
 EOF
 
 echo "--------------------------------------------"
-echo "配置:"
-echo -e "    端口:\033[36m         ${vport}\033[0m"
-echo -e "    UUID:\033[36m         ${vuuid}\033[0m"
-echo -e "    alterId:\033[36m      64\033[0m"
+echo -e "\33[31m\33[1m配置\33[0m:"
+echo -e "    端口:\033[36m          ${vport}\033[0m"
+echo -e "    UUID:\033[36m          ${vuuid}\033[0m"
+echo -e "    alterId:\033[36m       64\033[0m"
 echo -e "    websocket路径:\033[36m /${v_ws}\033[0m"
 echo -e "    域名:\033[36m          ${vurl}\033[0m"
 echo -e "    邮箱:\033[36m          ${vemail}\033[0m"
 echo "---------------------------------------------"
 echo " "
-echo -e "  \33[31m\33[1m执行\33[0m下面的命令替换配置文件"
-echo -e "替换v2ray配置文件:\033[36m mv ./v2ray.json /etc/v2ray/config.json \033[0m"
-echo -e "替换caddy配置文件:\033[36m mv ./Caddy.file /usr/local/caddy/Caddyfile \033[0m"
-echo "客户端配置文件v2ray-kehu.json复制到电脑上的客户端使用"
+echo -e "当前目录下已生成配置文件:"
+echo -e "   \033[36m v2ray-kehu.json \033[0m是客户端配置文件,复制到电脑上的客户端使用"
+echo -e "   \033[36m v2ray.json \033[0m是v2ray服务端配置文件"
+echo -e "   \033[36m Caddy.file \033[0m是Caddy的配置文件"
+echo " "
+echo -e "\33[31m\33[1m执行\33[0m下面的命令替换服务端配置文件:"
+echo -e "    替换v2ray配置文件:\033[36m mv ./v2ray.json /etc/v2ray/config.json \033[0m"
+echo -e "    替换caddy配置文件:\033[36m mv ./Caddy.file /usr/local/caddy/Caddyfile \033[0m"
+echo -e "    \33[31m\33[1m注意\033[0m,替换配置文件后需要重启软件使配置生效"
 
 cat > ./v2ray-kehu.json<<-EOF
 {
