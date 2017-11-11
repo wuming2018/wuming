33cc795cff99c515d23823f8e2e73b38cc97f53b
wuming <kai2017@qq.com>
2017-11-12 00:54:53 +0800

    test

--- v2-centos6.sh
+++ v2-centos6.sh
@@ -6,10 +6,19 @@
 # github:https://github.com/wuming2018
 # -------------------------
 
+uninstall() {
+  killall v2ray
+  [ -d /usr/bin/v2ray ] && rm -r /usr/bin/v2ray
+  [ -f /etc/v2ray/config.json ] && rm -r /etc/v2ray/config.json
+  grep -q "nohup /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json" /etc/rc.d/rc.local && sed -i "/nohup \/usr\/bin\/v2ray\/v2ray -config \/etc\/v2ray\/config.json/d" /etc/rc.d/rc.local
+}
+
+[[ $1 == "uninstall" ]] && uninstall && exit 0 
+
+echo "安装依赖 curl 和 unzip"
 yum install -y curl unzip
 
 VER="$(curl -s https://api.github.com/repos/v2ray/v2ray-core/releases/latest | grep 'tag_name' | cut -d\" -f4)"
-
 ARCH=$(uname -m)
 VDIS="64"
 
@@ -26,16 +35,18 @@ DOWNLOAD_LINK="https://github.com/v2ray/v2ray-core/releases/download/${VER}/v2ra
 rm -rf /tmp/v2ray
 mkdir -p /tmp/v2ray
 
-    echo "Downloading ${DOWNLOAD_LINK} directly."
-    curl -L -H "Cache-Control: no-cache" -o "/tmp/v2ray/v2ray.zip" ${DOWNLOAD_LINK}
+echo "下载和解压官方安装包"
+echo "Downloading ${DOWNLOAD_LINK} directly."
+curl -L -H "Cache-Control: no-cache" -o "/tmp/v2ray/v2ray.zip" ${DOWNLOAD_LINK}
 
 echo "Extracting V2Ray package to /tmp/v2ray."
 unzip "/tmp/v2ray/v2ray.zip" -d "/tmp/v2ray/"
 
 mkdir -p /var/log/v2ray
-
 mkdir -p /usr/bin/v2ray
 cp "/tmp/v2ray/v2ray-${VER}-linux-${VDIS}/v2ray" "/usr/bin/v2ray/v2ray"
+cp "/tmp/v2ray/v2ray-${VER}-linux-${VDIS}/geoip.dat" "/usr/bin/v2ray/geoip.dat"
+cp "/tmp/v2ray/v2ray-${VER}-linux-${VDIS}/geosite.dat" "/usr/bin/v2ray/geosite.dat"
 chmod +x "/usr/bin/v2ray/v2ray"
 
 mkdir -p /etc/v2ray
@@ -47,11 +58,13 @@ if [ ! -f "/etc/v2ray/config.json" ]; then
 
   UUID=$(cat /proc/sys/kernel/random/uuid)
   sed -i "s/23ad6b10-8d1a-40f7-8ad0-e3e35cd38297/${UUID}/g" "/etc/v2ray/config.json"
-
-  echo "PORT:${PORT}"
-  echo "UUID:${UUID}"
+  echo "配置文件:"
+  echo "     端口: ${PORT}"
+  echo "     UUID: ${UUID}"
+  echo "  AlterID: 64"
 fi
 
+[ ! -f "/usr/bin/v2ray/v2ray" ] && echo "出错了,没有成功安装v2ray到/usr/bin/v2ray/" && exit 1
 # 加入到开机启动
 grep -q "nohup /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json" /etc/rc.d/rc.local || echo -e "nohup /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json >/dev/null 2>/dev/null &" >>/etc/rc.d/rc.local
 # 清除 v2ray 进程
@@ -60,12 +73,10 @@ killall v2ray
 #nohup /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json >/dev/null 2>/dev/null &
 
 echo "V2Ray ${VER} is installed."
-echo "配置文件:"
-echo "     端口: ${PORT}"
-echo "     UUID: ${UUID}"
-echo "  AlterID: 64"
-echo "--------------"
+echo "---------------------------------------------------------------------------------------------"
 echo -e "配置文件在/etc/v2ray/config.json"
+echo -e "更新:   bash v2-centos6.sh"
+echo -e "卸载:   bash v2-centos6.sh uninstall"
 echo -e "停止:   killall v2ray"
 echo -e "运行:   nohup /usr/bin/v2ray/v2ray -config /etc/v2ray/config.json >/dev/null 2>/dev/null &"
 echo -e "注意:先 停止 然后再 运行,不然可能会在后台多次运行v2ray进程"
